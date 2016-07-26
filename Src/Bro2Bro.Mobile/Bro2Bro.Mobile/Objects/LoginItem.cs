5e1867326d8a5557a94e09ac0c41a2c01957b37e
Jarred Capellman <jcapellman@hotmail.com>
2016-07-25 22:35:14 -0400

    Updated the Manifest and removed DataContract/DataMember in the LoginItem

--- Src/Bro2Bro.Mobile/Bro2Bro.Mobile/Objects/LoginItem.cs
+++ Src/Bro2Bro.Mobile/Bro2Bro.Mobile/Objects/LoginItem.cs
@@ -1,12 +1,7 @@
-﻿using System.Runtime.Serialization;
-
-namespace Bro2Bro.Mobile.Objects {
-    [DataContract]
-    public class LoginItem {
-        [DataMember]
+﻿namespace Bro2Bro.Mobile.Objects {    
+    public class LoginItem {        
         public string Username { get; set; }
-
-        [DataMember]
+        
         public string Password { get; set; }
     }
 }
