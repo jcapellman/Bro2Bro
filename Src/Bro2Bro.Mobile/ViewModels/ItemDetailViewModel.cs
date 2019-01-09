fb9324dbd687c83fb14b47357cbf801cd9544f63
Jarred Capellman <jcapellman@hotmail.com>
2019-01-08 20:06:04 -0600

    Added: Initial Commit of the new App/WebAPI

---
+++ Src/Bro2Bro.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Bro2Bro.Mobile.Models;
+
+namespace Bro2Bro.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
