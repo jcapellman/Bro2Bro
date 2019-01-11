9d1b5212bddd410557b3bb36b254bf4e9a968751
Jarred Capellman <jcapellman@hotmail.com>
2019-01-10 22:00:15 -0600

    Added: UWP Device Implementation

---
+++ Src/Bro2Bro.Mobile.UWP/Implementations/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Windows.Devices.Enumeration;
+
+using Bro2Bro.Mobile.Interfaces;
+
+namespace Bro2Bro.Mobile.UWP.Implementations
+{
+    public class Device : IDevice
+    {
+        public async Task<string> GetDeviceAsync()
+        {
+            var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync();
+
+            return devices.FirstOrDefault(a => a.Kind == DeviceInformationKind.Device && a.IsEnabled)?.Id;
+        }
+    }
+}
