c8f71d094f5fe70461b265028eadb1cd83afb088
Jarred Capellman <jcapellman@hotmail.com>
2019-01-10 21:59:10 -0600

    Added: IDevice Interface

---
+++ Src/Bro2Bro.Mobile/Interfaces/IDevice.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Bro2Bro.Mobile.Interfaces
+{
+    public interface IDevice
+    {
+        Task<string> GetDeviceAsync();
+    }
+}
--- Src/Bro2Bro.Mobile/ViewModels/ItemDetailViewModel.cs
+++ Src/Bro2Bro.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -1,16 +1,55 @@
-﻿using Bro2Bro.Mobile.Models;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
-namespace Bro2Bro.Mobile.ViewModels
+using Bro2Bro.Mobile.Models;
+
+using Xamarin.Forms;
+
+namespace Bro2Bro.Mobile.ViewModels 
 {
     public class ItemDetailViewModel : BaseViewModel
     {
-        public Item Item { get; set; }
+        public ObservableCollection<Item> Items { get; set; }
+
+        public Command LoadItemsCommand { get; set; }
 
         public ItemDetailViewModel(Item item = null)
         {
             Title = item?.BroName;
 
-            Item = item;
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+
+                var items = await DataStore.GetItemsAsync(true);
+
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
