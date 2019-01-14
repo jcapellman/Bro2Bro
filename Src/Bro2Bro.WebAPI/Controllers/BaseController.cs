8f9790165b7caacf01561a5fbb99d5a74398f587
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 19:16:14 -0600

    Added: IDatabase to the BaseController constructor #7

--- Src/Bro2Bro.WebAPI/Controllers/BaseController.cs
+++ Src/Bro2Bro.WebAPI/Controllers/BaseController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bro2Bro.lib.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bro2Bro.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     public class BaseController : Controller
     {
+        protected readonly IDatabase Database;
+
+        public BaseController(IDatabase iDatabase)
+        {
+            Database = iDatabase;
+        }
     }
 }
