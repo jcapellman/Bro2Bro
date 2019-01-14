d7ea34145f1593aab047804c49f26746fef5b534
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 22:13:35 -0600

    Added: Initial work on the Messages Table

---
+++ Src/Bro2Bro.lib/DAL/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Bro2Bro.lib.DAL.Base;
+
+namespace Bro2Bro.lib.DAL
+{
+    public class Messages : BaseTable
+    {
+        public string ReceiverBroId { get; set; }
+
+        public string SenderBroId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Content { get; set; }
+    }
+}
