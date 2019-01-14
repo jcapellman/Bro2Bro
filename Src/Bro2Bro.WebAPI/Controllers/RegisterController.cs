658e1f172575df996ff8d3d3ff837e24898bff80
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 20:02:34 -0600

    Added: Initial work on the RegisterController class #15

---
+++ Src/Bro2Bro.WebAPI/Controllers/RegisterController.cs
@@ -0,0 +1,14 @@
+﻿using Bro2Bro.lib.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bro2Bro.WebAPI.Controllers
+{
+    public class RegisterController : BaseController
+    {
+        public RegisterController(IDatabase iDatabase) : base(iDatabase) { }
+
+        [HttpPost]
+        public bool Register(string broId, string displayName) => Database.RegisterBro(broId, displayName);
+    }
+}
