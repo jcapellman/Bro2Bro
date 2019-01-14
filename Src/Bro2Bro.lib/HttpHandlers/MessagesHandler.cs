de1a8f4ddb2fca3930c3becc1e7dbf89e81a9f2c
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 22:14:20 -0600

    Added: MessagesHandler class #9

---
+++ Src/Bro2Bro.lib/HttpHandlers/MessagesHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Bro2Bro.lib.DAL;
+using Bro2Bro.lib.HttpHandlers.Base;
+
+namespace Bro2Bro.lib.HttpHandlers
+{
+    public class MessagesHandler : BaseHttpHandler
+    {
+        public MessagesHandler(string baseWebServiceUrl) : base(baseWebServiceUrl)
+        {
+        }
+
+        public async Task<List<Messages>> GetMessagesAsync(string senderBroId, string receiverBroId) =>
+            await GetAsync<List<Messages>>("Messages", senderBroId, receiverBroId);
+    }
+}
