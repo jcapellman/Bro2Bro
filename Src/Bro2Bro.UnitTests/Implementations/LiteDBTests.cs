d3356e630c7fe35155f1d9fc33401e49443ff312
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 19:37:19 -0600

    Added: Initial work on the LiteDbTests #14

---
+++ Src/Bro2Bro.UnitTests/Implementations/LiteDBTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+using Bro2Bro.lib.Implementations;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bro2Bro.UnitTests.Implementations
+{
+    [TestClass]
+    public class LiteDbTests
+    {
+        [TestMethod]
+        public void GetBrosNull()
+        {
+            var db = new LiteDbDatabase();
+
+            var result = db.GetBros(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(!result.Any());
+        }
+
+        [TestMethod]
+        public void GetBrosEmpty()
+        {
+            var db = new LiteDbDatabase();
+
+            var result = db.GetBros(string.Empty);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(!result.Any());
+        }
+
+        [TestMethod]
+        public void GetBrosNotFound()
+        {
+            var db = new LiteDbDatabase();
+
+            var result = db.GetBros("notfound");
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(!result.Any());
+        }
+
+        [TestMethod]
+        public void GetBrosFound()
+        {
+            var db = new LiteDbDatabase();
+
+            var result = db.GetBros("found");
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
+            Assert.IsTrue(result.Count == 1);
+        }
+    }
+}
