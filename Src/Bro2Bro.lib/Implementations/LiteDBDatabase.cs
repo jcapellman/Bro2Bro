eca56c3cb8b8818928953d416b779daed6d6565d
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 19:24:32 -0600

    Added: Initial commit of LiteDbDatabase #7

---
+++ Src/Bro2Bro.lib/Implementations/LiteDBDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Bro2Bro.lib.DAL;
+using Bro2Bro.lib.Interfaces;
+
+using LiteDB;
+
+namespace Bro2Bro.lib.Implementations
+{
+    public class LiteDbDatabase : IDatabase
+    {
+        private readonly string _connectionString = "main.db";
+        
+        public List<Bros> GetBros(string searchQuery)
+        {
+            using (var liteDb = new LiteDatabase(_connectionString))
+            {
+                return liteDb.GetCollection<Bros>().Find(a => a.DisplayName.Contains(searchQuery)).ToList();
+            }
+        }
+    }
+}
