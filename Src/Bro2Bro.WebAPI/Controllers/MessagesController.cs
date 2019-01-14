2825911330ff523b8c82c7d7249757ebb7315519
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 22:13:06 -0600

    Added: Initial work on the MessagesController class #9

---
+++ Src/Bro2Bro.WebAPI/Controllers/MessagesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Bro2Bro.lib.DAL;
+using Bro2Bro.lib.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bro2Bro.WebAPI.Controllers
+{
+    public class MessagesController : BaseController
+    {
+        public MessagesController(IDatabase iDatabase) : base(iDatabase)
+        {
+        }
+
+        [HttpGet]
+        public List<Messages> GetMessages(string senderBroId, string receiverBroId) =>
+            Database.GetMessages(senderBroId, receiverBroId);
+    }
+}
