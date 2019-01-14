4797c42995f99dd263d7923a74e528f344babccf
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 20:33:18 -0600

    Added: Initial work on the RegisterHandler class #17

---
+++ Src/Bro2Bro.lib/HttpHandlers/RegisterHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Bro2Bro.lib.HttpHandlers.Base;
+
+namespace Bro2Bro.lib.HttpHandlers
+{
+    public class RegisterHandler : BaseHttpHandler
+    {
+        public async Task<bool> RegisterAsync(string broId, string displayName) => await PostAsync<bool>("/Register", broId, displayName);
+
+        public RegisterHandler(string serverConnection) : base(serverConnection)
+        {
+        }
+    }
+}
