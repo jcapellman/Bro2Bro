ac00d3c5b99f97660fd4ee87a51058ed305dcfbc
Jarred Capellman <jcapellman@hotmail.com>
2019-01-13 19:22:00 -0600

    Added: BaseTable class #13

---
+++ Src/Bro2Bro.lib/DAL/BaseTable.cs
@@ -0,0 +1,9 @@
+﻿namespace Bro2Bro.lib.DAL
+{
+    public class BaseTable
+    {
+        public long Id { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ Src/Bro2Bro.lib/DAL/Bros.cs
@@ -0,0 +1,9 @@
+﻿namespace Bro2Bro.lib.DAL
+{
+    public class Bros : BaseTable
+    {
+        public string BroID { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
