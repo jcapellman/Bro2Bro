fd2a1ad651ea33d933a2e1f0f3fe5c1e6c63fd52
Jarred Capellman <jcapellman@hotmail.com>
2019-01-14 19:13:26 -0600

    Change: FindBroHandlerTests now inherits from BaseHttpTests

--- Src/Bro2Bro.UnitTests/HttpHandlers/FindBroHandlerTests.cs
+++ Src/Bro2Bro.UnitTests/HttpHandlers/FindBroHandlerTests.cs
@@ -1,16 +1,15 @@
 ﻿using System.Threading.Tasks;
 
 using Bro2Bro.lib.HttpHandlers;
+using Bro2Bro.UnitTests.HttpHandlers.Base;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bro2Bro.UnitTests.HttpHandlers
 {
     [TestClass]
-    public class FindBroHandlerTests
+    public class FindBroHandlerTests : BaseHttpTests
     {
-        private const string SERVER_BASE_URL = "http://localhost:5000/api/";
-
         [TestMethod]
         public async Task NullTest()
         {
