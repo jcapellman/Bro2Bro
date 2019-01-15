1452f76a9877f25f21e616203c73db0d76e3b1ef
Jarred Capellman <jcapellman@hotmail.com>
2019-01-14 19:14:04 -0600

    Added: MessagesHandlerTests class #18

---
+++ Src/Bro2Bro.UnitTests/HttpHandlers/MessagesHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+using Bro2Bro.lib.HttpHandlers;
+using Bro2Bro.UnitTests.HttpHandlers.Base;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bro2Bro.UnitTests.HttpHandlers
+{
+    [TestClass]
+    public class MessagesHandlerTests : BaseHttpTests
+    {
+        [TestMethod]
+        public async Task NullArguments()
+        {
+            var messageHandler = new MessagesHandler(SERVER_BASE_URL);
+
+            var result = await messageHandler.SendMessageAsync(null, null);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
