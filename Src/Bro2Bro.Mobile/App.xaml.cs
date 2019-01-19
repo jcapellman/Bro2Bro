d5b816708425cee0d63420dca1d81d5a5637698a
Jarred Capellman <jcapellman@hotmail.com>
2019-01-18 22:25:02 -0600

    Change: Some refactoring of the App class

--- Src/Bro2Bro.Mobile/App.xaml.cs
+++ Src/Bro2Bro.Mobile/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 using Bro2Bro.Mobile.Services;
 using Bro2Bro.Mobile.Views;
 
@@ -9,8 +9,10 @@ namespace Bro2Bro.Mobile
 {
     public partial class App : Application
     {
-        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
-        public static string AzureBackendUrl = "http://localhost:5000";
+        public static string ProductionBackendUrl = "http://localhost:5000";
+
+        public static string BroId = string.Empty;
+
         public static bool UseMockDataStore = true;
 
         public App()
@@ -18,26 +20,15 @@ namespace Bro2Bro.Mobile
             InitializeComponent();
 
             if (UseMockDataStore)
+            {
                 DependencyService.Register<MockDataStore>();
+            }
             else
-                DependencyService.Register<AzureDataStore>();
+            {
+                DependencyService.Register<ProductionDataStore>();
+            }
 
             MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
         }
     }
 }
