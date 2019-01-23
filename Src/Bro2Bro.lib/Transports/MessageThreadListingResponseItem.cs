111866064e8afb54b6a09a34e06449a999029ec2
Jarred Capellman <jcapellman@hotmail.com>
2019-01-22 20:01:10 -0600

    Added: Initial work on the MessageThreadListingResponseItem class

---
+++ Src/Bro2Bro.lib/Transports/MessageThreadListingResponseItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bro2Bro.lib.Transports
+{
+    public class MessageThreadListingResponseItem
+    {
+        public string BroId { get; set; }
+
+        public DateTime LastMessage { get; set; }
+
+        public int MessageCount { get; set; }
+    }
+}
