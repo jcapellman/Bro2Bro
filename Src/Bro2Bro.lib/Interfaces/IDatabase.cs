3fd4e5c4d7e7e32f7968eb659dc81d49105a96bd
Jarred Capellman <jcapellman@hotmail.com>
2019-01-22 20:02:57 -0600

    Added: GetMessageThreads declaration in the IDatabase class

--- Src/Bro2Bro.lib/Interfaces/IDatabase.cs
+++ Src/Bro2Bro.lib/Interfaces/IDatabase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 
 using Bro2Bro.lib.DAL;
+using Bro2Bro.lib.Transports;
 
 namespace Bro2Bro.lib.Interfaces
 {
@@ -15,5 +16,7 @@ namespace Bro2Bro.lib.Interfaces
         List<Messages> GetMessages(string senderBroId, string receiverBroId);
 
         bool SendMessage(string senderBroId, string receiverBroId, string content);
+
+        List<MessageThreadListingResponseItem> GetMessageThreads(string receiverBroId);
     }
 }
