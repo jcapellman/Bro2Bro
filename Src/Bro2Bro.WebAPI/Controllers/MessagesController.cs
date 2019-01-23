0181bb9b1919a2a6f9f3d7c42fe1a068306d4646
Jarred Capellman <jcapellman@hotmail.com>
2019-01-22 20:01:43 -0600

    Added: GetMessageThreads method to the MessagesController class

--- Src/Bro2Bro.WebAPI/Controllers/MessagesController.cs
+++ Src/Bro2Bro.WebAPI/Controllers/MessagesController.cs
@@ -2,6 +2,8 @@
 
 using Bro2Bro.lib.DAL;
 using Bro2Bro.lib.Interfaces;
+using Bro2Bro.lib.Transports;
+
 using Bro2Bro.WebAPI.Auth;
 
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +17,9 @@ namespace Bro2Bro.WebAPI.Controllers
         [HttpGet]
         public List<Messages> GetMessages(string senderBroId, string receiverBroId) =>
             Database.GetMessages(senderBroId, receiverBroId);
+
+        [HttpGet]
+        public List<MessageThreadListingResponseItem> GetMessageThreads() => Database.GetMessageThreads(HttpRequestContext.BroId);
 
         [HttpPost]
         public bool SendMessage(string receiverBroId, string content) => Database.SendMessage(string.Empty, receiverBroId, content);
